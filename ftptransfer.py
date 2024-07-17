110a0caea12fa5da465086b06a802c854fb335c1
Josh <agrey127@gmail.com>
2024-07-17 11:48:44 -0400

    added message time limit and delete

--- ftptransfer.py
+++ ftptransfer.py
@@ -2,7 +2,7 @@ from O365 import Account, FileSystemTokenBackend, mailbox
 import time
 import runjson
 import startbatch
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import threading
@@ -20,6 +20,10 @@ account = Account(credentials, auth_flow_type='credentials', tenant_id='REDACTED
 
 currentMonth = datetime.now().month
 currentYear = datetime.now().year
+currentDay = datetime.now().day
+threeminago = datetime.now() - timedelta(minutes=3)
+
+
 #Path = "C:/Users/ITGURU/PycharmProjects/JSON/"
 #Path = "C:/Users/ITGURU/PycharmProjects/JSON/"
 def my_function():
@@ -28,9 +32,14 @@ def my_function():
         print('Authenticated!')
         mailbox = account.mailbox('jrobinson@actresearch.net')
         inbox = mailbox.inbox_folder()
+        query = mailbox.new_query()
+        queryresult = query.on_attribute('created_date_time').greater(
+            datetime(threeminago.year, threeminago.month, threeminago.day, threeminago.hour, threeminago.minute))
+        filtered_messages = mailbox.get_messages(query=queryresult)
 
-        for message in inbox.get_messages():
+        for message in filtered_messages:
             messagetocheck = str(message)
+
 
             if "U.S. Trailers Report" in messagetocheck:
                 startbatch.runbatch()
@@ -52,6 +61,7 @@ def my_function():
                 time.sleep(5)
                 runjson.runbatch()
                 print("JSON Ran Successfully")
+                message.delete()
 
 
 
@@ -61,6 +71,7 @@ def my_function():
                 time.sleep(5)
                 runjson.runbatch2()
                 print("JSON Ran Successfully")
+                message.delete()
 
 def run_function():
     thread = threading.Timer(60.0, run_function) # 60 seconds = 1 minute
